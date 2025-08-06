24f51a991b7a0f8cd5c326970a9b946c1204a092
Adarsh-rg <adarshrg@webforcefive.com.au>
2025-08-07 01:07:21 +0530

    git changes

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: mode === 'production' ? '/Portfolio/' : '/',
+  base: '/Adarsh/Portfolio',
   plugins: [
     react(),
   ],
